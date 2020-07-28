152739cce10766493656b75933f79061c9d8af8a
Fraciscv <Francisco.Andaur@icloud.com>
2020-07-27 21:22:09 -0400

    working on user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if @user = User.find_by(username: params[:username]@user.authenticate (params[:password)]
-          session[:user_id] = @user.id
-          flash[:notice] = "login succesfull! Welcome #{user.name}"
+    if @user = User.find_by(username: params[:username])
+      @user.authenticate (params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "login succesfull! Welcome #{user.name}"
       redirect_to @user
   else flash[:notice] = "incorrect username or password"
       redirect_to login_path 
+    end
   end
 
   def destroy
     session.delete(:user_id)
-    redirect_to posts_url, notice: 'Logged out!'
+    redirect_to login_path, notice: 'Logged out!'
   end
 end
