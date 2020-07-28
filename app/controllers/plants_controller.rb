95fe75e4c58aa271373dde0a0b1ea8f16a479471
21shield <21netaly@gmail.com>
2020-07-27 20:22:23 -0400

    added plant validations and new controller methods

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,22 @@
 class PlantsController < ApplicationController
+
+    def new
+        @plant = Plant.new
+    end
+     
+    def create
+        @plant = Plant.new(plant_params)
+        if @plant.save
+            redirect_to garden_path
+        else
+            render :new
+        end
+    end
+    
+    private
+    
+    def plant_params
+        params.require(:plant).permit(:plant_type, :nickname, :size, :water_amount, :garden_id)
+    end
+    
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,4 +3,16 @@ class Plant < ApplicationRecord
   has_many :tasks
   has_many :users, through: :tasks
   has_many :users, through: :garden
+
+  validates :plant_type, :size, :water_amount, presence: true
+  
+
+  # t.string "plant_type"
+  # t.string "nickname"
+  # t.integer "size"
+  # t.integer "water_ammount"
+
+  
+
+
 end
