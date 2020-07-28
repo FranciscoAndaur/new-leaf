b07d89ae3d5430a123e1188c06046929f3fc6893
21shield <21netaly@gmail.com>
2020-07-28 12:07:02 -0400

    fixed garden path

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,31 +1,7 @@
 class GardensController < ApplicationController
 
-    def mygarden
-        # should show the user their garden if they have one
-      
-       @garden = Garden.find_by(user_id: current_user.id)
-        #other wise create new garden
-   
-        if !!@garden
-         
-            redirect_to garden_path
-        else
-            # page rendered says you currently have no plants in your garden
-            # maybe have a garden created as you sign up
-            # render a page that allows you either add a plant remove a plant /homebutton/ logoutbutton 
-            redirect_to new_garden_path
-        end    
-    end
-
-    def new
-        @garden = Garden.new
-        # one of these has to build out the form
-        @garden.plants.build
-        @garden.plants.build
-        @garden.plants.build
-    end
-    def create
-        @garden = Garden.new(garden_params)
+    def index
+        @garden = Garden.find_by(user_id: current_user.id)
     end
 
     private
@@ -33,7 +9,7 @@ class GardensController < ApplicationController
     def garden_params
         # waterammount <<< new spelling
         # needs to have nested attributes for plant
-        params.require(:garden).permit(:garden_name,:plant_pop, plants_attributes:[:plant_type, :nickname, :size, :water_ammount])
+        params.require(:garden).permit(:garden_name,:plant_pop)
     end
   
 
