d9aa32a4d8cb51f424f77c19aabab050e428781c
21shield <21netaly@gmail.com>
2020-07-28 01:08:26 -0400

    take it back now

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -3,7 +3,10 @@ class GardensController < ApplicationController
     def garden
        @garden = Garden.find_by(user_id: current_user.id)
     end
+
     private
+
+    
   
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,9 @@ class UsersController < ApplicationController
   before_action :find_user, only: [:show, :edit, :update, :destroy]
   skip_before_action :authorize, only: [:new, :create]
 
-  def settings
+  def show
     #find_user
-    @user = current_user
-    
+    render :show
   end
 
   def edit
